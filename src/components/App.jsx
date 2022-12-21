9e74724e16fdb9eb893f7302d35000858eda0ae5
NilKad <kadulin@gmail.com>
2022-12-21 22:15:29 +0200

    v1

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+// import { useEffect, useRef } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from 'components/ContactList/ContactList';
+import { useSelector } from 'react-redux';
+// import { getAllContacts } from './API/fetchContacts';
+
+export const App = () => {
+  const isLoading = useSelector(state => state.contacts.isLoading);
+  const error = useSelector(state => state.contacts.error);
+
+  return (
+    <div>
+      {isLoading && '...Loading...'}
+      {error && error}
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </div>
+  );
+};
